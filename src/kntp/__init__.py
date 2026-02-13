2fd5cc69d315312c8ac445c33b0ec1dcd89e093c
도형 <taehu_u@naver.com>
2026-02-13 16:39:45 +0900

    Restructure project to package layout

---
+++ src/kntp/__init__.py
@@ -0,0 +1,12 @@
+from .core import (
+    DEFAULT_BASE,
+    DEFAULT_SERVERS,
+    Sample,
+    Stats,
+    Ranked,
+    query_ntp,
+    collect_stats,
+    rank_servers,
+    recommend,
+    grade,
+)
